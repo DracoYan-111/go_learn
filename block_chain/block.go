bfbc111f41315bc5ab987e0dee35a37890a9c360
Draco Yan <2944972991@qq.com>
2023-10-27 12:06:45 +0800

    Improve more details

--- block_chain/block.go
+++ block_chain/block.go
@@ -3,7 +3,6 @@ package block_chain
 import (
 	"bytes"
 	"crypto/sha256"
-	"encoding/binary"
 	"strconv"
 	"time"
 )
@@ -17,28 +16,27 @@ type Block struct {
 	Hash      []byte // The hash of the current block.
 }
 
-// SetHash calculates and sets the hash value of the block.
-func (b *Block) SetHash() {
+// getHash calculates the hash of a given block.
+func getHash(b *Block) []byte {
+	// Convert the index to a byte slice.
+	index := []byte(strconv.FormatInt(b.Index, 10))
 
 	// Convert the timestamp to a byte slice.
 	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
-	// Create a new byte slice to store the headers.
-	index := make([]byte, 8)
-	binary.LittleEndian.PutUint64(index, uint64(b.Index))
+
 	// Concatenate the previous hash, data, and timestamp.
 	headers := bytes.Join([][]byte{index, b.PrevHash, b.Data, timestamp}, []byte{})
 
-	// Calculate the hash using SHA256.
+	// Calculate the SHA256 hash of the concatenated headers.
 	hash := sha256.Sum256(headers)
 
-	// Set the hash value of the block.
-	b.Hash = hash[:]
+	return hash[:]
 }
 
 // NewBlock creates a new block with the given data and previous hash.
 // It returns a pointer to the newly created block.
 func NewBlock(index int64, data string, prevHash []byte) *Block {
-	// Create a new block object with the current timestamp, data, previous hash,
+	// Create a new block object with the current index+1, timestamp, data, previous hash,
 	// and an empty hash field.
 	block := &Block{
 		Index:     index + 1,
@@ -48,8 +46,8 @@ func NewBlock(index int64, data string, prevHash []byte) *Block {
 		Hash:      []byte{},
 	}
 
-	// Calculate and set the hash of the block using the SetHash method.
-	block.SetHash()
+	// Calculate the hash value of the block using the GetHash method
+	block.Hash = getHash(block)
 
 	return block
 }
@@ -59,4 +57,30 @@ func NewBlock(index int64, data string, prevHash []byte) *Block {
 func NewGenesisBlock() *Block {
 	// Create a new block with the data "Genesis Block" and an empty byte slice as the previous hash.
 	return NewBlock(0, "Genesis Block", []byte{})
+}
+
+// IsBlockValid checks if a block is valid
+func IsBlockValid(prveBlock *Block, newBlock *Block) bool {
+
+	// Compare the index of the previous block to the index of the new block.
+	if prveBlock.Index+1 != newBlock.Index {
+		return false
+	}
+
+	// Compare the hash of the previous block to the hash of the new block.
+	if !bytes.Equal(prveBlock.Hash, newBlock.PrevHash) {
+		return false
+	}
+
+	// Compare the hash of the previous block to the hash of the previous block.
+	if !bytes.Equal(getHash(prveBlock), prveBlock.Hash) {
+		return false
+	}
+
+	// Compare the hash of the new block to the hash of the new block.
+	if !bytes.Equal(getHash(newBlock), newBlock.Hash) {
+		return false
+	}
+
+	return true
 }
--- block_chain/block_chain.go
+++ block_chain/block_chain.go
@@ -4,16 +4,37 @@ type Blockchain struct {
 	Blocks []*Block
 }
 
+// AddBlock adds a new block to the blockchain with the given data.
 func (bc *Blockchain) AddBlock(data string) {
-	prveBlock := bc.Blocks[len(bc.Blocks)-1]
-	newBlock := NewBlock(prveBlock.Index, data, prveBlock.Hash)
-	bc.Blocks = append(bc.Blocks, newBlock)
+	// Get the previous block in the blockchain.
+	previousBlock := bc.Blocks[len(bc.Blocks)-1]
+
+	// Create a new block with the index, data, and hash of the previous block.
+	newBlock := NewBlock(previousBlock.Index, data, previousBlock.Hash)
+
+	// Check if the new block is valid by comparing it with the previous block.
+	if IsBlockValid(previousBlock, newBlock) {
+		// Append the new block to the blockchain.
+		bc.Blocks = append(bc.Blocks, newBlock)
+		replaceChain(bc, bc.Blocks)
+	}
 }
 
+// NewBlockchain creates a new instance of the Blockchain struct.
 func NewBlockchain() *Blockchain {
-	return &Blockchain{[]*Block{NewGenesisBlock()}}
+	// Create a new Blockchain instance and initialize it with a single block, the genesis block.
+	blockchain := &Blockchain{
+		Blocks: []*Block{NewGenesisBlock()},
+	}
+	// Return the newly created blockchain instance.
+	return blockchain
 }
 
-// func GetBlockchain() *Blockchain {
-// 	return blockchain
-// }
+// replaceChain replaces the blocks in the blockchain with a new chain if the new chain is longer.
+func replaceChain(bc *Blockchain, chain []*Block) {
+	// Check if the length of the new chain is greater than the length of the current blockchain's blocks.
+	if len(chain) > len(bc.Blocks) {
+		// If true, replace the blockchain's blocks with the new chain.
+		bc.Blocks = chain
+	}
+}
